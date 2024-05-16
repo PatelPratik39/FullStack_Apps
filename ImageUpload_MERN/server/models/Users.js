b47a4544d1df8a131cd5aac54b6f4bfe87423079
PatelPratik39 <ptl.pratik717@gmail.com>
2024-05-16 17:43:53 -0500

    app removed

--- ImageUpload_MERN/server/models/Users.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  image: String
-});
-
-const UserModel = mongoose.model("user", UserSchema);
-module.exports = UserModel;
--- MovieReviewApp/ReelReviewsApp/src/API/filters.js
+++
@@ -1,9 +0,0 @@
-import apiRequest from ".";
-
-export const GetQuerySearchFilterResults = async (payload) => {
-  return await apiRequest({
-    method: "GET",
-    endPoint: "/api/filters",
-    queryStrings: payload
-  });
-};
