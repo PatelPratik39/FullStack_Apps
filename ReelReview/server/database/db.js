34618068430bc860c4bf7374dfe83337e4eeb5c4
PatelPratik39 <ptl.pratik717@gmail.com>
2024-05-17 10:43:26 -0500

    mongo URI removed

--- ReelReview/server/database/db.js
+++ ReelReview/server/database/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URI);
-console.log("MongoDB URI:", process.env.MONGO_URL);
 const connection = mongoose.connection;
 
 connection.on("connected", () => {
