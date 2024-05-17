c6f26572b12f7f459d071301995bf6ee89fd720f
PatelPratik39 <ptl.pratik717@gmail.com>
2024-05-17 10:31:09 -0500

    dotenv

--- ReelReview/server/database/db.js
+++ ReelReview/server/database/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URI);
+console.log("MongoDB URI:", process.env.MONGO_URL);
 const connection = mongoose.connection;
 
 connection.on("connected", () => {
